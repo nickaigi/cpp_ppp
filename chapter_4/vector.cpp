8271da10810316481da3cccdb7388b5452edbc59
Nickson Kaigi <nickaigi@gmail.com>
2019-03-27 22:35:37 +0300

    added a sort operation to the vector

--- chapter_4/vector.cpp
+++ chapter_4/vector.cpp
@@ -1,18 +1,38 @@
+#include<algorithm>
 #include<iostream>
 #include<vector>
 using namespace std;
 
 
 int main(){
-    vector<int> v1 {1,2,3,4,5};
+    string name;
+    vector<int> v1 {8,2,3,9,1};
     vector<string> names;
 
+    cout<<"Our vector contains:\n";
     for(int i: v1)
         cout<<i<<endl;
 
-    cout<<"please enter your names: ";
-    for(string name; cin>>name;)
+    sort(v1.begin(), v1.end()); // sort in ascending order
+
+    cout<<"After sorting the vector\n";
+    for(int i: v1)
+        cout<<i<<endl;
+
+    cout<<"Adding dynamic values to a vector:\n";
+    for(int i = 0; i < 10; ++i)
+        names.push_back("person-" + to_string(i));
+
+    for(string person: names)
+        cout<<person<<endl;
+
+    cout<<"Please provide other names to add to the vector:\n";
+    for(string name;cin>>name;) // notice that the for loop doen't include the step
         names.push_back(name);
+
+    sort(names.begin(), names.end());
+
+    cout<<"After sorting the names:\n";
 
     cout<<"Names entered: "<<names.size()<<endl;
     for(string name:names)
