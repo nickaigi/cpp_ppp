b15ca09973ed03d8790fac13c175b2635b5ab892
Nickson Kaigi <nickaigi@gmail.com>
2019-04-02 06:08:31 +0300

    working on calc, added headers and example code from Bjarne S

--- chapter_6/calc.cpp
+++ chapter_6/calc.cpp
@@ -11,25 +11,90 @@ class Token{
 Token get_token();
 
 inline void error(const string&);
+double expression();
+double term();
+double primary();
 
 vector<Token> tok;
 
 int main(){
-    while(cin){
-        Token t = get_token();
-        tok.push_back(t);
+    try{
+        while(cin)
+            cout<<expression()<<'\n';
     }
-    for(Token t: tok)
-        cout<<t.kind<<' '<<t.value;
-    //for(int i = 0; i < tok.size(); ++i){
-    //    if(tok[i].kind == '*'){
-    //        double d = tok[i-1].value * tok[i+1].value
-    //    }
-    //}
+    catch(exception& e){
+        cerr<<e.what()<<'\n';
+        return 1;
+    }
+    catch(...){
+        cerr<<"exception \n";
+        return 2;
+    }
 }
 
 // error() simply disguises throws:
 inline void error(const string& s)
 {
 	throw runtime_error(s);
+}
+
+double expression(){
+    double left = term();
+    Token t = get_token();
+    while(true){
+        switch(t.kind){
+            case '+':
+                left += term();
+                t = get_token();
+                break;
+            case '-':
+                left -= term();
+                t = get_token();
+                break;
+            default:
+                return left;
+        }
+    }
+}
+
+double term(){
+    double left = primary();
+    Token t =  get_token();
+    while(true){
+        switch(t.kind){
+            case '*':
+                left *= primary();
+                t = get_token();
+                break;
+            case '/':
+            {   // if you want to define and initialize variables within a switch
+                // you must place them inside a block
+                double d = primary();
+                if(d == 0) error("divide by zero");
+                left /= d;
+                t = get_token();
+                break;
+            }
+            default:
+                return left;
+        }
+    }
+}
+
+double primary(){
+    Token t = get_token();
+    switch(t.kind){
+        case '(':
+            {
+                double d = expression();
+                t = get_token();
+                if(t.kind != ')')
+                    error("')' expected");
+                return d;
+            }
+        case '8': //we use 8 to represent a number
+            return t.value;
+        default:
+            error("primary expected");
+    }
 }
